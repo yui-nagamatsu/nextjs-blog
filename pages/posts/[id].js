3a02e140175327390e05ad0067cf3b2747241280
yui <79505155+yui-nagamatsu@users.noreply.github.com>
2021-03-05 10:22:49 +0900

    スペースを調整しただけ

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,23 +4,6 @@ import { getAllPostIds,getPostData } from '../../lib/posts'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
-    return {
-        props: {
-            postData
-        }
-    }
-}
-
-export async function getStaticPaths(){
-    const paths = getAllPostIds()
-    return {
-        paths,
-        fallback: false
-    }
-}
-
 export default function Post({ postData }){
     return (
         <Layout>
@@ -38,3 +21,19 @@ export default function Post({ postData }){
     )
 }
 
+export async function getStaticPaths(){
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
