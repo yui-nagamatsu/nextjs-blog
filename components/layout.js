4b85a2148323c27db4363634a223a65730e26cdc
yui <79505155+yui-nagamatsu@users.noreply.github.com>
2021-03-06 16:36:23 +0900

    Reflect the change of the profile image again

--- components/layout.js
+++ components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="../images/blog-profile.jpg"
+              src="/images/blog-profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
