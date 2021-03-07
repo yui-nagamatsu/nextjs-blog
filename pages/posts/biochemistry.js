5f4811a9f6859e58a2b4afe34113a0d2906c02d1
yui <79505155+yui-nagamatsu@users.noreply.github.com>
2021-03-07 23:32:59 +0900

    Navigate between home and biochemistry

---
+++ pages/posts/biochemistry.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Biochemistry() {
+    return (
+      <>
+        <h2>生化学</h2>
+        <p>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </p>
+      </>
+    )
+  }
