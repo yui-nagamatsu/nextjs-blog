5fc1f61c06047548b751969c2203037ac75cc734
yui <79505155+yui-nagamatsu@users.noreply.github.com>
2021-03-08 01:29:50 +0900

    Import layout to categoryItem

--- pages/posts/english.js
+++ pages/posts/english.js
@@ -1,14 +1,19 @@
 import Link from 'next/link'
+import utilStyles from '../../styles/utils.module.css'
+import Layout from '../../components/layout'
 
 export default function English() {
     return (
-      <>
-        <h2>医学英語</h2>
-        <p>
-          <Link href="/">
-            <a>Back to home</a>
-          </Link>
-        </p>
-      </>
+        <div>
+            <Layout home></Layout>
+            <>
+            <h2 className={utilStyles.headingLg}>生化学</h2>
+            <p>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+            </p>
+            </>
+        </div>
     )
   }
--- pages/posts/physiology.js
+++ pages/posts/physiology.js
@@ -1,14 +1,19 @@
 import Link from 'next/link'
+import utilStyles from '../../styles/utils.module.css'
+import Layout from '../../components/layout'
 
 export default function Physiology() {
     return (
-      <>
-        <h2>生理学</h2>
-        <p>
-          <Link href="/">
-            <a>Back to home</a>
-          </Link>
-        </p>
-      </>
+        <div>
+            <Layout home></Layout>
+            <>
+            <h2 className={utilStyles.headingLg}>生化学</h2>
+            <p>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+            </p>
+            </>
+        </div>
     )
   }
