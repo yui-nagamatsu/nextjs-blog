4a005a8ca116db5c8cd15d0c41045e1005416079
yui <79505155+yui-nagamatsu@users.noreply.github.com>
2021-03-07 23:42:51 +0900

    Navigate between home, Physiology and English

---
+++ pages/posts/english.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function English() {
+    return (
+      <>
+        <h2>医学英語</h2>
+        <p>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </p>
+      </>
+    )
+  }
---
+++ pages/posts/physiology.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Physiology() {
+    return (
+      <>
+        <h2>生理学</h2>
+        <p>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </p>
+      </>
+    )
+  }
