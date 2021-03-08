f0fb426d9799238245fd1dc229c0b15addac4ad6
yui <79505155+yui-nagamatsu@users.noreply.github.com>
2021-03-08 20:42:34 +0900

    Add margin to global.css

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,7 +11,7 @@ export default function Post({ postData }){
                 <title>{postData.title}</title>
             </Head>
             <article>
-            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <h2 className={utilStyles.headingXl}>{postData.title}</h2>
             <div className={utilStyles.lightText}>
                 <Date dateString={postData. date} />     
             </div>
