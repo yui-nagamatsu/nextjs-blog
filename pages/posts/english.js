6857b31c2b7d26341ba4bb28bd88e103ee7cdb09
yui <79505155+yui-nagamatsu@users.noreply.github.com>
2021-03-10 02:42:48 +0900

    Add backtohome to each category page

--- pages/posts/english.js
+++ pages/posts/english.js
@@ -4,10 +4,15 @@ import Layout from '../../components/layout'
 
 export default function English() {
     return (
+        <div className={utilStyles.biochemistryEntire}>
             <Layout>
-                <>
-                <h2 className={utilStyles.headingLg}>生化学</h2>
-                </>
+                <h2 className={utilStyles.headingLg}>ENGLISH</h2>
             </Layout>
+            <div className={utilStyles.backToHome}>
+            <Link href="/">
+            <a><i class="fas fa-home"></i> BACK TO HOME</a>
+            </Link>
+            </div>
+      </div>
     )
   }
--- pages/posts/physiology.js
+++ pages/posts/physiology.js
@@ -4,10 +4,15 @@ import Layout from '../../components/layout'
 
 export default function Physiology() {
     return (
-        <Layout>
-            <>
-            <h2 className={utilStyles.headingLg}>生化学</h2>
-            </>
-        </Layout>
+        <div className={utilStyles.biochemistryEntire}>
+            <Layout>
+                <h2 className={utilStyles.headingLg}>PHYSIOLOGY</h2>
+            </Layout>
+            <div className={utilStyles.backToHome}>
+            <Link href="/">
+            <a><i class="fas fa-home"></i> BACK TO HOME</a>
+            </Link>
+            </div>
+      </div>
     )
   }
